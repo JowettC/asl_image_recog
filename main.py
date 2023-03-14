98b13a685df5c65f47df31c674810f883da01974
j0kene <jolene.yeoo@gmail.com>
2023-03-15 04:37:17 +0800

    Integrated with model

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import datetime, time
 import os, sys
 import numpy as np
 from threading import Thread
-
+import tensorflow as tf
 
 global capture, grey, switch, neg, face, out 
 capture=0
 grey=0
 neg=0
 face=0
-switch=1
+# -1: initialisation, 0: stopped, 1: running 
+switch=-1
 
 
 #make shots directory to save pics
@@ -22,16 +23,16 @@ try:
 except OSError as error:
     pass
 
-#Load pretrained face detection model    
-net = cv2.dnn.readNetFromCaffe('./models/deploy.prototxt.txt', './models/res10_300x300_ssd_iter_140000.caffemodel')
+# Load pretrained model    
+
+#net = cv2.dnn.readNetFromCaffe('./models/deploy.prototxt.txt', './models/res10_300x300_ssd_iter_140000.caffemodel')
+model = tf.keras.models.load_model('./models/model4/my_model_4')
 
 #instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
 
 
 camera = cv2.VideoCapture(0)
-
-
 
 # def detect_face(frame):
 #     global net
@@ -56,13 +57,19 @@ camera = cv2.VideoCapture(0)
 #     except Exception as e:
 #         pass
 #     return frame
- 
+
+# Load the CNN model
+
+def getLetter(result):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','k','L','m','n','o','p','q','r','s','t','u','v','w','x','y']
+    return alphabet[int(result)]
 
 def gen_frames():  # generate frame by frame from camera
     global out, capture
     while True:
         success, frame = camera.read() 
         if success:
+            frame = cv2.flip(frame,1)
             if(grey):
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             if(neg):
@@ -74,13 +81,31 @@ def gen_frames():  # generate frame by frame from camera
                 cv2.imwrite(p, frame)
                 
             try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                # define region of interest
+                roi= frame[100:320, 100:320]
+                cv2.rectangle(frame, (100,100), (320,320), (255,0,0), 5)
+
+                roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+                roi = cv2.resize(roi,(28,28), interpolation = cv2.INTER_AREA)              
+                roi= roi.reshape(1,28,28,1)
+                roi = roi/255
+
+                model_predict = model.predict(roi, 1, verbose = 0)
+                model_classes = np.argmax(model_predict, axis=1)
+                result = str(model_classes[0])
+
+                cv2.putText(frame, getLetter(result), (300,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+
+                # Convert the frame to a JPEG image
+                ret, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
+
                 yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+       
             except Exception as e:
                 pass
-                
         else:
             pass
 
@@ -88,7 +113,6 @@ def gen_frames():  # generate frame by frame from camera
 @app.route('/')
 def index():
     return render_template('index.html')
-    
     
 @app.route('/video_feed')
 def video_feed():
@@ -107,11 +131,6 @@ def tasks():
         elif  request.form.get('neg') == 'Negative':
             global neg
             neg=not neg
-        elif  request.form.get('face') == 'Face Only':
-            global face
-            face=not face 
-            if(face):
-                time.sleep(4)   
         elif  request.form.get('stop') == 'Stop/Start':
             
             if(switch==1):
@@ -122,16 +141,15 @@ def tasks():
             else:
                 camera = cv2.VideoCapture(0)
                 switch=1
-
                           
                  
-    elif request.method=='GET':
+        elif request.method=='GET':
+            return render_template('index.html')
         return render_template('index.html')
-    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
     
 camera.release()
 cv2.destroyAllWindows()     
